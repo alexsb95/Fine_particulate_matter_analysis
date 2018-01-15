43b3965e193c1bb3b41219ec32f510b32b62ba3e
alexsb95 <alexsb95@gmail.com>
2018-01-14 18:00:21 -0600

    Project created

---
+++ plot1.R
@@ -0,0 +1,5 @@
+readFiles <- function(){
+  ## This first line will likely take a few seconds. Be patient!
+  NEI <- readRDS("summarySCC_PM25.rds")
+  SCC <- readRDS("Source_Classification_Code.rds")
+}
